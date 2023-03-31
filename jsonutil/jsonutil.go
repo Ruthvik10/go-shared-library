9d16f6333c7dc1135c34872f15f28df22da51e2c
Ruthvik <rk99219@gmail.com>
2023-04-01 01:51:51 +0530

    added json utilites

---
+++ jsonutil/jsonutil.go
@@ -0,0 +1,39 @@
+package jsonutil
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+func WriteJSON(w http.ResponseWriter, data any, status int, headers http.Header) {
+	out, err := json.Marshal(data)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	out = append(out, '\n')
+	for k, v := range headers {
+		w.Header()[k] = v
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(out)
+}
+
+func ReadJSON(w http.ResponseWriter, r *http.Request, dest any) error {
+	const MAX_BYTES = 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(MAX_BYTES))
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	err := dec.Decode(dest)
+	if err != nil {
+		return err
+	}
+	err = dec.Decode(&struct{}{})
+	if !errors.Is(err, io.EOF) {
+		return errors.New("body must contain a single JSON value")
+	}
+	return nil
+}
